3d51269f8cf9176204bf20bef83238836cc86370
samuel <sbacon803@gmail.com>
2022-10-04 08:26:57 -0600

    remove extraneaous text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -44,21 +44,6 @@
             <div class="inner">
               <h2>{{cart.name}}</h2>
               <p>{{ cart.location }}</p>
-              <ul class="actions">
-                <li><a href="generic.html" class="button">Learn more</a></li>
-              </ul>
-            </div>
-          </div>
-        </section>
-        <section>
-          <a href="#" class="image"><img src="images/pic02.jpg" alt="" data-position="top center" /></a>
-          <div class="content">
-            <div class="inner">
-              <h2>Feugiat consequat</h2>
-              <p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-              <ul class="actions">
-                <li><a href="generic.html" class="button">Learn more</a></li>
-              </ul>
             </div>
           </div>
         </section>
@@ -66,11 +51,6 @@
           <a href="#" class="image"><img src="images/pic03.jpg" alt="" data-position="25% 25%" /></a>
           <div class="content">
             <div class="inner">
-              <h2>Ultricies aliquam</h2>
-              <p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-              <ul class="actions">
-                <li><a href="generic.html" class="button">Learn more</a></li>
-              </ul>
             </div>
           </div>
         </section>
@@ -80,7 +60,6 @@
       <section id="three" class="wrapper style1 fade-up">
         <div class="inner">
           <h2>Owners Login/ Signup</h2>
-          <p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet imperdiet est velit quis lorem.</p>
           <div class="split style1">
             <section>
               <form method="post" action="#">
